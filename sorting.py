4c4d0bdffa8dbbacd54742a5862a87009ccf4b22
Jan Sháněl <279847@vutbr.cz>
2026-04-20 12:59:53 +0200

    Update

--- sorting.py
+++ sorting.py
@@ -8,6 +8,27 @@ print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
 
 small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
 
+def selection_sort(values):
+    for min_index in range(len(values)):
+        min_value = values[min_index]
+        min_ind = min_index
+        for i in range(min_index, len(values)):
+            if values[i] < min_value:
+                min_ind = i
+                min_value = values[i]
+        values[min_ind], values[min_index] = values[min_index], values[min_ind]
+
+    return values
+
+def main():
+    num_list = [5, 1, 4, 2, 8]
+    print(selection_sort(num_list))
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
