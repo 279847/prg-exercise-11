7609ec7f6dba6ed78fe654db6afdd8992d63b8d8
Jan Sháněl <279847@vutbr.cz>
2026-04-20 13:38:37 +0200

    Update

--- sorting.py
+++ sorting.py
@@ -1,5 +1,7 @@
 import random
 
+import matplotlib as plt
+
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
@@ -20,9 +22,34 @@ def selection_sort(values):
 
     return values
 
+def bubble_sort(values):
+    values = list(values)
+    plt.ion()
+    plt.show()
+    for i in range(len(values)):
+        for val in range(len(values) - 1):
+            if values[val] > values[val + 1]:
+                    values[val], values[val + 1] = values[val + 1], values[val]
+            index_highlight1 = val
+            index_highlight2 = val + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+    plt.ioff()
+    plt.show()
+
+    return values
+
+
+
 def main():
     num_list = [5, 1, 4, 2, 8]
     print(selection_sort(num_list))
+    print(bubble_sort(num_list))
 
 
 if __name__ == "__main__":
